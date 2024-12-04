b4b54f7732faeed610189ca4659d02c07b8a3efe
NelshCode <nelsonhulois@gmail.com>
2024-12-04 17:28:20 +0100

    chore: comment

--- tests/dbtest.ts
+++ tests/dbtest.ts
@@ -1,9 +1,7 @@
 import Database from 'better-sqlite3';
-
-// Connexion à la base SQLite (le fichier `players.db` sera créé s'il n'existe pas)
+// use :memory to keep in RAM
 const db_test = new Database(":memory:", { verbose: console.log });
 
-// Création de la table des joueurs
 db_test.exec(`
   CREATE TABLE IF NOT EXISTS players (
     pseudo TEXT PRIMARY KEY,
@@ -11,5 +9,4 @@ db_test.exec(`
   );
 `);
 
-// Export de la connexion pour l'utiliser dans d'autres fichiers
 export default db_test;
