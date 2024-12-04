9a6db66048b0ca4f18f2787f493e296c9ba32474
NelshCode <nelsonhulois@gmail.com>
2024-12-04 17:29:00 +0100

    chore: to english

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import playerRoutes from './playerRoutes';
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); // Middleware pour parser les JSON
-app.use('/api', playerRoutes); // Utilisation des routes
+app.use(express.json()); // Middleware to parse JSON
+app.use('/api', playerRoutes); // routes usage
 
 // Lancer le serveur
 app.listen(PORT, () => {
